ce1779623b445b317781e45dce490b72e2134e19
Lee SH <snghyun331@gmail.com>
2023-11-09 12:04:37 +0900

    Fix: DB_PORT의 parseInt문법 수정

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -15,7 +15,7 @@ import { AppService } from './app.service';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
+      port: parseInt(process.env.DB_PORT, 10),
       username: process.env.DB_USER,
       password: process.env.DB_PW,
       database: process.env.DB_NAME,
