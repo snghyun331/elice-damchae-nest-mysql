0f03883412538e2a65ba37d50c6750e0312b2a4a
Lee SH <snghyun331@gmail.com>
2023-11-12 00:36:06 +0900

    feat: 이미지 엔티티 생성 및 관계 설정

--- back/src/users/entity/user.entity.ts
+++ back/src/users/entity/user.entity.ts
@@ -2,10 +2,13 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
 import { Mbti } from './common/user.enums';
+import { ImageEntity } from 'src/images/entity/image.entity';
 
 @Entity('User')
 export class UserEntity {
@@ -24,11 +27,8 @@ export class UserEntity {
   @Column({ type: 'varchar', unique: true, nullable: false })
   nickname: string;
 
-  @Column({ type: 'varchar' })
-  profileImg: string;
-
-  @Column({ type: 'varchar' })
-  mbtiImg: string;
+  @Column({ type: 'varchar', nullable: true })
+  mbtiImg?: string;
 
   @Column({ type: 'boolean', default: false, nullable: false })
   isOut: boolean;
@@ -41,4 +41,8 @@ export class UserEntity {
 
   @UpdateDateColumn()
   updatedAt: Date;
+
+  @OneToOne(() => ImageEntity, { nullable: true })
+  @JoinColumn({ name: 'profileImg' })
+  profileImg?: ImageEntity;
 }
